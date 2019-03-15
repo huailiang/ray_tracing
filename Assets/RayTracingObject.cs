264ea608ed8cdea6b308c1074f2f2e45fd8680f6
David Kuri <info@davidkuri.de>
2019-03-15 14:41:39 +0100

    Data structures / data for triangle meshes and actual tracing code.

---
+++ Assets/RayTracingObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RayTracingObject : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        RayTracingMaster.RegisterObject(this);
+    }
+
+    private void OnDisable()
+    {
+        RayTracingMaster.UnregisterObject(this);
+    }
+}
